57eb77291ebaa3f4e620ea8552ab28a2e8263d83
Tenzin Woeser <tenzin.woeser@swish.ca>
2024-10-30 13:57:41 -0400

    fix: added new route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", (req, res) => {
+  res.send("New User Route Available");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
