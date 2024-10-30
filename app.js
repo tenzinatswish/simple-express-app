dd16981fcbc41fe38e497650a72aa461723dd181
Tenzin Woeser <tenzin.woeser@swish.ca>
2024-10-30 14:09:12 -0400

    new route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/users", (req, res) => {
+app.get("/api/users", (req, res) => {
   res.send("New User Route Available");
 });
 
